b70a14db2735103e4e11f62b3158f43d8bd70879
Lucas Craveiro <lucasgutierrecraveiro@outlook.com>
2024-06-18 17:49:23 -0300

    fix(layout): add more margin top to main page title

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="flex flex-col   ">
-      <div className="text-2xl font-bold text-center">
+    <div className="flex flex-col">
+      <div className="text-2xl font-bold text-center mt-4">
         <span>Simple coin search</span>
       </div>
       <div>{children || <Outlet />}</div>
